98af208a1d28f3fb151bd2d40419f27a4a2fce03
tomasramadan <tomasramadan4@gmail.com>
2022-09-25 19:25:34 -0300

    Final

--- form.php
+++ form.php
@@ -1,18 +1,19 @@
 <?php
 
-$nombre = $_POST["nombre"];
-$correo = $_POST["correo"];
-$telefono = $_POST["telefono"];
-$mensaje = $_POST["mensaje"];
+$nombre = $_POST['nombre'];
+$correo = $_POST['correo'];
+$telefono = $_POST['telefono'];
+$mensaje = $_POST['mensaje'];
 
 $mensaje = "Este mensaje fue enviado por " . $nombre , ",\r\n";
 $mensaje = "Con la dirección de correo " . $correo , ",\r\n";
 $mensaje = "Mensaje " . $_POST["mensaje"] , ",\r\n";
 $mensaje = "Enviado el " . date("d/m/y", time());
 
-$destinatario = "tomasramadan4@gmail.com";
-$asunto = "Cerrajeria David";
+$destinatario = 'tomasramadan4@gmail.com';
+$asunto = 'Cerrajeria David'
 
 mail($destinatario, $asunto, utf8_decode($mensaje), $header);
-include "cotizacion.html";
+header('Location:index.html');
+
 ?>
