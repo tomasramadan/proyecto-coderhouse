0629532665d6103aa416a730c55f11df97184978
tomasramadan <tomasramadan4@gmail.com>
2022-09-25 18:14:33 -0300

    Aspectos finales

---
+++ php/form.php
@@ -0,0 +1,18 @@
+<?php
+
+$nombre = $_POST["nombre"];
+$correo = $_POST["correo"];
+$telefono = $_POST["telefono"];
+$mensaje = $_POST["mensaje"];
+
+$mensaje = "Este mensaje fue enviado por " . $nombre , ",\r\n";
+$mensaje = "Con la dirección de correo " . $correo , ",\r\n";
+$mensaje = "Mensaje " . $_POST["mensaje"] , ",\r\n";
+$mensaje = "Enviado el " . date("d/m/y", time());
+
+$destinatario = "tomasramadan@gmail.com";
+$asunto = "Cerrajeria David";
+
+mail($destinatario, $asunto, utf8_decode($mensaje), $header);
+header("Location:cotizacion.html")
+?>
